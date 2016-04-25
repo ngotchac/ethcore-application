b5c983b4778836bf8bde74643708484a4ae64ed5
Nicolas Gotchac <ngotchac@gmail.com>
2016-04-25 23:59:16 +0200

    First Commit -- Fleshing up the React App

---
+++ app/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Job from '../advert';
+
+export default class App extends React.Component {
+    render() {
+        console.log(Job);
+        return (<div>
+            <h1>Ethcore Application</h1>
+            <h2>Nicolas Gotchac</h2>
+        </div>);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+var path = require('path'),
+    merge = require('webpack-merge'),
+    webpack = require('webpack'),
+    HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Whether the command is `npm start` or `npm build`
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style', 'css'],
+                include: PATHS.app
+            },
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: PATHS.app
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'node_modules/html-webpack-template/index.ejs',
+            title: 'Ethcore - Job Application',
+            appMountId: 'app',
+            inject: false
+        })
+    ]
+};
+
+// When Developing
+if(TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            // Only display errors to reduce the amount of output
+            stats: 'errors-only',
+
+            host: process.env.HOST,
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if(TARGET === 'build') {
+    module.exports = merge(common, {});
+}
